32568e1380afd6a390250ab827053e8da9e295f4
Dawid Matuszewski <dawmatuszewski@gmail.com>
2017-11-05 22:44:15 +0100

    Zad. 18

---
+++ client/MessageList.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styles from './MessageList.css';
+
+const Message = props => (
+  <div className={styles.Message}>
+    <p><span className={styles.ClientName}>{props.from} :</span></p>
+    <span>{props.text}</span>
+  </div>
+);
+
+const MessageClient = props => (
+  <div className={styles.MessageClient}>
+    <p><span className={styles.ClientName}>{props.from} :</span></p>
+    <span>{props.text}</span>
+  </div>
+);
+
+const MessageList = props => (
+  <div className={styles.MessageList}>
+    {
+      props.messages.map((message, i) => {
+        if (message.from === props.client) {
+        return (
+          <Message
+            key={i}
+            from='Me'
+            text={message.text}
+          />
+          );
+        } else {
+        return (
+          <MessageClient
+            key={i}
+            from={message.from}
+            text={message.text}
+          />
+          );
+        }
+      })
+    }
+  </div>
+);
+
+export default MessageList;
